1c6060467546d3b97cb5d9846d98c558bdb79f82
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-12-06 16:27:07 +0100

    home 2

---
+++ src/app/home-2/page.tsx
@@ -0,0 +1,23 @@
+import AboutSection from '@/components/AboutSection';
+import BeforeAndAfter from '@/components/BeforeAndAfter';
+import Hero from '@/components/Hero';
+import ServicesSection from '@/components/ServicesSection';
+import beforeImage from '@/../public/before-and-after/before.webp';
+import afterImage from '@/../public/before-and-after/after.webp';
+import DentalTourismSection from '@/components/DentalTourismSection';
+import CtaSection from '@/components/CtaSection';
+import { Gallery } from '@/components/gallery';
+
+export default function Home() {
+  return (
+    <main className='bg-[rgba(78,78,78,1)] -z-50'>
+      <Hero />
+      <AboutSection />
+      <Gallery className='my-28' />
+      <ServicesSection className='pb-0 pt-16 sm:py-24 lg:py-20 px-4 sm:px-8 lg:px-16 overflow-hidden ' />
+      <BeforeAndAfter beforeImage={beforeImage} afterImage={afterImage} />
+      <DentalTourismSection />
+      <CtaSection/>
+    </main>
+  );
+}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import heroImage from '@/../public/hero.jpeg';
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-[60svh] lg:h-[100svh] flex flex-col justify-end pt-0 px-4 sm:px-16 sm:pb-40 lg:px-44 lg:pb-24 text-white before:content-[""] before:absolute before:bottom-0 before:left-1/2 before:-translate-x-1/2 before:w-[110vw] before:h-[calc(100svh+2px)] before:-z-10 before:bg-background hero-clip-path overflow-hidden'>
+    <section className='relative w-full h-[60svh] lg:h-[100svh] flex flex-col justify-end pt-0 px-4 sm:px-16 sm:pb-40 lg:px-44 lg:pb-24 text-white before:content-[""] before:absolute before:bottom-0 before:left-1/2 before:-translate-x-1/2 before:w-[110vw] before:h-[calc(100svh+2px)] before:z-10 before:bg-background hero-clip-path overflow-hidden'>
       <Image
         src={heroImage}
         alt='Grand Dental klinika'
@@ -12,13 +12,13 @@ const Hero = () => {
         sizes='100vw'
         quality={85}
         priority
-        className='object-contain lg:object-cover -z-20 object-top lg:object-[100%_30%] lg:height-750:object-[100%_20%] aspect-[828/552] lg:aspect-auto max-w-full'
+        className='object-contain lg:object-cover object-top lg:object-[100%_30%] lg:height-750:object-[100%_20%] aspect-[828/552] lg:aspect-auto max-w-full'
       />
-      <h1 className='text-5xl lg:text-6xl'>Dobrodošli u Grand Dental</h1>
-      <h4 className='mt-4 text-2xl'>
+      <h1 className='text-5xl lg:text-6xl z-10'>Dobrodošli u Grand Dental</h1>
+      <h4 className='mt-4 text-2xl z-10'>
         Naša misija je da vam stvorimo holivudski osmeh, bez filtera.
       </h4>
-      <Button href='/kontakt' className='mt-8'>
+      <Button href='/kontakt' className='mt-8 z-10'>
         Zakažite pregled
       </Button>
     </section>
