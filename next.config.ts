0bed8b88dd728bc8918d16635e44ddb55b887774
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2025-03-12 12:04:11 +0100

    image optimization formats

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   env: {
     GOOGLE_MAPS_API_KEY: 'AIzaSyBQlagxaq90oXM_y4WjMw0_MsqoxPm-zAg',
   },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 export default nextConfig;
